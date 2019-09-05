cc7b267b3080de87893ace2f313914a371f2cd8f
djl57 <18367345931@163.com>
2019-09-05 14:55:49 +0800

    0905

--- src/view/blog/App/index.js
+++ src/view/blog/App/index.js
@@ -14,45 +14,6 @@ import AccountSettingsPage from "./accountSettingsPage/index";
 import MyBlogPage from "./myBlogPage/index";
 import BlogManagementPage from "./blogManagementPage/index";
 
-const SUCCESS = "200";
-
-class PrivateRoute extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      authPass: false
-    };
-  }
-  async componentWillMount() {
-    const res = await checkAuth(this.props.location.pathname);
-    if (res && res.code === SUCCESS) {
-      this.setState({
-        authPass: true
-      });
-    }
-  }
-  render() {
-    const { component: Component, ...rest } = this.props;
-    return (
-      <Route
-        {...rest}
-        render={props =>
-          this.state.authPass ? (
-            <Component {...props} />
-          ) : (
-            <Redirect
-              to={{
-                pathname: "/app/login",
-                state: { from: props.location }
-              }}
-            />
-          )
-        }
-      />
-    );
-  }
-}
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -66,15 +27,15 @@ class App extends React.Component {
           <Switch>
             <Route path="/app/home" component={HomePage} />
             <Route path="/app/blog" component={BlogPage} />
-            <PrivateRoute path="/app/writeBlog" component={WriteBlogPage} />
-            <PrivateRoute path="/app/message" component={MessagePage} />
+            <Route path="/app/writeBlog" component={WriteBlogPage} />
+            <Route path="/app/message" component={MessagePage} />
             <Route path="/app/login" component={LoginPage} />
             <Route path="/app/register" component={RegisterPage} />
-            <PrivateRoute path="/app/myCollection" component={MyCollectionPage} />
-            <PrivateRoute path="/app/personalCenter" component={PersonalCenterPage} />
-            <PrivateRoute path="/app/accountSettings" component={AccountSettingsPage} />
-            <PrivateRoute path="/app/myBlog" component={MyBlogPage} />
-            <PrivateRoute path="/app/blogManagement" component={BlogManagementPage} />
+            <Route path="/app/myCollection" component={MyCollectionPage} />
+            <Route path="/app/personalCenter" component={PersonalCenterPage} />
+            <Route path="/app/accountSettings" component={AccountSettingsPage} />
+            <Route path="/app/myBlog" component={MyBlogPage} />
+            <Route path="/app/blogManagement" component={BlogManagementPage} />
           </Switch>
         </Router>
       </div>
